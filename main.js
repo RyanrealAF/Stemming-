5ab8dff30ad471d09e6818c3054d8bfbd5a3c072
purarecoveryryan <purarecoveryryan@gmail.com>
2026-03-03 02:54:00 +0000

    iddjn

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./ui/App.jsx";
-import "./ui/styles.css";
+import App from "./App.jsx";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
